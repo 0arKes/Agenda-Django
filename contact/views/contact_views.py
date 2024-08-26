81fde6af6b3d0de3286f1f748f464d667b909f7b
Bruno Haag <proarkes@gmail.com>
2024-08-26 13:39:26 -0300

    new pages
    Modificação e separação dos arquivos html em blocos, implementação da barra de pesquisa com consulta ao DB

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 # Create your views here.
 
@@ -7,6 +8,32 @@ def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value)
+        ).order_by('-id')[:10]
+    
+    context = {
+        'contacts': contacts,
+        'site_title':  'search - ',
     }
 
     return render(
@@ -21,6 +48,7 @@ def contact(request, contact_id):
     
     context = {
         'contact': single_contact,
+        'site_title': f'{single_contact.first_name} {single_contact.last_name} -'
     }
 
     return render(
