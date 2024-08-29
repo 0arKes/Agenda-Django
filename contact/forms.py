90f6d2a6dff55368b885765351b99f9a3ffc1ca0
Bruno Haag <proarkes@gmail.com>
2024-08-29 18:50:21 -0300

    Mod forms
    Modificação do formulário, criação de erros (teste) nos campos do forms, salvando dados no DB

--- contact/forms.py
+++ contact/forms.py
@@ -3,17 +3,48 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+            widget=forms.TextInput(
+                attrs={
+                    'placeholder': 'teste22'
+                }
+            ),
+            label='Primeiro Nome',
+            help_text='usu'
+        )
     class Meta:
+        
+
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone')
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category')
 
     
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError('mensagem de erro', code='invalid')
-        )
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                )
+            )
+
+        return first_name
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -5,12 +5,18 @@ from contact.forms import ContactForm
 def create(request):
 
     if request.method == 'POST':
-        context = {'form':ContactForm(request.POST)}
+        form = ContactForm(request.POST)
+        context = {'form': form}
+         
+        if form.is_valid():
+            form.save()
+
         return render(
             request,
             'contact/create.html',
             context,
         )
+        
 
     context = {'form':ContactForm()}
     return render(
