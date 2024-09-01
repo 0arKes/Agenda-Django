d94d6690d4b711208b0dd36b2463cc9f3389e15c
Bruno Haag <proarkes@gmail.com>
2024-09-01 11:14:30 -0300

    Picture
    implementação de imagens ao contato e modificação de imagens através do formulário

--- contact/forms.py
+++ contact/forms.py
@@ -2,21 +2,25 @@ from django import forms
 from django.core.exceptions import ValidationError
 from . import models
 
+
 class ContactForm(forms.ModelForm):
     first_name = forms.CharField(
-            widget=forms.TextInput(
-                attrs={
-                    'placeholder': 'teste22'
-                }
-            ),
-            label='Primeiro Nome',
-            help_text='usu'
+            label='First Name',
+            help_text='Ex: Bruno'
         )
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*'
+            }
+        ))
+    
+
     class Meta:
         
 
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category')
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture')
 
     
     def clean(self):
@@ -34,6 +38,7 @@ class ContactForm(forms.ModelForm):
             self.add_error('last_name', msg)
 
         return super().clean()
+    
     
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
